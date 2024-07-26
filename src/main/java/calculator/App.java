98aee395135f339cc7c9bfebc50965733b6777ef
Woojin1123 <boom175839@gmail.com>
2024-07-26 20:31:46 +0900

    Level 1 : requirement#7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -8,8 +8,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
-        int[] results = new int[10]; // int형 배열 선언 후 size 10 으로 초기화
-        int index = 0; // 위치를 저장할 index
+        ArrayList<Integer> results = new ArrayList<>(); // int타입 wrapper Integer ArrayList 선언 및 초기화
 
         while(true) { //while(true)를 이용해 break전까지 무한반복
             System.out.println("숫자 연산자 숫자 순으로 입력하세요.(입력값 공백으로 구분) "); // 입력 한번에 받음
@@ -31,20 +30,18 @@ public class App {
                     case '*' -> result = firstNumber * secondNumber;
                 }
                 System.out.printf("%d %c %d 결과 : %d\n", firstNumber, operator, secondNumber, result);
-                results[index] = result; // 계산 결과를 results 의 index번째에 저장
-                index++;// 다음 저장 위치로 이동
-                if(index == 10){ //배열이 꽉 찼을 경우
-                    for(int i = 0; i<9; i++){
-                        results[i] = results[i+1]; // 한칸씩 앞으로 당겨옴
-                    }
-                    index = 9; // index를 배열의 마지막 위치를 가리키도록 바꿔 새로 저장 가능하게 함
-                }
+                results.add(result); //ArrayList의 add메소드를 사용해 저장
                 for(int i : results) { // foreach문을 활용해 results 내용 출력
                     System.out.printf("%d ",i);
                 }
                 System.out.println();
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                if(sc.next().equals("exit")){break;} // sc.next()로 입력버퍼에 존재하는 문자열 받아온 후 String의 equals메소드로 exit와 값 비교
+                System.out.println("remove 입력 시 첫번째 결과 삭제 (exit 입력 시 종료)");
+                String func = sc.next(); // sc.next()로 비교하면 입력 버퍼의 문자열이 사라지므로 String에 담아서 입력값 확인
+                if(func.equals("exit")){
+                    break;
+                } else if(func.equals("remove")){
+                    results.remove(0);//ArrayList의 remove 메소드를 이용해 0번째 결과 값 삭제
+                }
             } catch (ArithmeticException e) {
                 System.out.println(e.getMessage());//e.getmessage()를 이용해 throw한 예외의 오류메세지 출력
             } //try-catch문 활용해서 예외 처리
