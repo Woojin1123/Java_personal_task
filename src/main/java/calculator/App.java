686bb98bc9caf7bffef9024659d52f03ea3360fb
Woojin1123 <boom175839@gmail.com>
2024-07-26 19:43:05 +0900

    Level 1 : requirement#1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,17 @@
 package calculator;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
+        int firstNumber = sc.nextInt(); //nextInt로 숫자 받아서 초기화
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        int secondNumber = sc.nextInt();
+        System.out.printf("%d %d",firstNumber,secondNumber);
     }
 }
