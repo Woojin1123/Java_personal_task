a01f34b68ff77f115c9d722e26214e5b53c3b290
Woojin1123 <boom175839@gmail.com>
2024-07-26 20:11:56 +0900

    Level 1 : requirement#5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,6 +8,9 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
+        int[] results = new int[10]; // int형 배열 선언 후 size 10 으로 초기화
+        int index = 0; // 위치를 저장할 index
+
         while(true) { //while(true)를 이용해 break전까지 무한반복
             System.out.println("숫자 연산자 숫자 순으로 입력하세요.(입력값 공백으로 구분) "); // 입력 한번에 받음
             int firstNumber = sc.nextInt(); //nextInt로 숫자 받아서 초기화
@@ -28,6 +31,12 @@ public class App {
                     case '*' -> result = firstNumber * secondNumber;
                 }
                 System.out.printf("%d %c %d 결과 : %d\n", firstNumber, operator, secondNumber, result);
+                results[index] = result; // 계산 결과를 results 의 index번째에 저장
+                index++;// 다음 저장 위치로 이동
+                for(int i : results) { // foreach문을 활용해 results 내용 출력
+                    System.out.printf("%d ",i);
+                }
+                System.out.println();
                 System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
                 if(sc.next().equals("exit")){break;} // sc.next()로 입력버퍼에 존재하는 문자열 받아온 후 String의 equals메소드로 exit와 값 비교
             } catch (ArithmeticException e) {
