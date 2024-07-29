d4af6244eb089597578327398e6a358d05c25cbf
Woojin1123 <boom175839@gmail.com>
2024-07-29 17:09:25 +0900

    Level 3 : requirement#3

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.stream.Stream;
+import org.w3c.dom.ls.LSOutput;
+
 public class ArithmeticCalculator<T extends Number> extends Calculator {//Number타입의 자손 즉, Float,Double,Integer 등 숫자만 가능하게함, 인스턴스 생성 시 타입을 타입 변수 <T>에 명시
   private Operator<T> op;
   public Operator<T> getOp() {
@@ -32,13 +35,18 @@ public class ArithmeticCalculator<T extends Number> extends Calculator {//Number
   public double calculate(T firstNumber, T secondNumber, char opChar) { //T타입이 App.java에서 지정한 타입으로 변경, 제네릭으로 선언되었기 때문에 App.java에서 입력될 타입을 지정하면 다른 클래스의 타입을 변경하지 않아도 됨.
     return makeOp(opChar).operate(firstNumber,secondNumber);// calculate는 연산기호가 추가되는것에 상관없이 operate만 수행
   }
-  public void lambdaResults(T num){ //stream & lambda를 사용하지 않았을 때
+  public void lambdaResults(T num){
+//    System.out.print(num + " 보다 큰 결과값 : ");
+//    for(double d : getResults()){
+//      if(d>(double)num){
+//        System.out.printf("%.2f ",d);
+//      }
+//    }
+//    System.out.println();
     System.out.print(num + " 보다 큰 결과값 : ");
-    for(double d : getResults()){
-      if(d>(double)num){
-        System.out.printf("%.2f ",d);
-      }
-    }
+    getResults().stream() // results ArrayList로 스트림생성
+        .filter( d -> d > num.doubleValue()) // 람다식을 이용해 d>num일때 결과값 필터링
+        .forEach(d -> System.out.printf("%.2f ", d)); //걸러진 d값 출력
     System.out.println();
   }
 }
