1a7cc8260bc5384810a4548c3eee39a6a36128d1
Woojin1123 <boom175839@gmail.com>
2024-07-29 11:39:58 +0900

    Fix : DivideOperator Exception throw

--- src/main/java/calculator/DivideOperator.java
+++ src/main/java/calculator/DivideOperator.java
@@ -3,6 +3,9 @@ package calculator;
 public class DivideOperator implements Operator {
   @Override
   public double operate(int firstNumber,int secondNumber){
+    if(secondNumber == 0){
+      throw new ArithmeticException("분모에는 0이 올 수 없습니다.");
+    }
     return  (double)firstNumber / secondNumber;
   }
 }
