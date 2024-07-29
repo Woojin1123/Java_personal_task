90c02680e3cd3f6dde3d85020d126bbc8b1397f4
Woojin1123 <boom175839@gmail.com>
2024-07-29 10:33:09 +0900

    Level 2 : requirement#7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,27 +10,46 @@ public class App {
         Calculator calcInstance = new Calculator(); // Calculator 생성자를 통해 results가 초기화됨
 
         while(true) { //while(true)를 이용해 break전까지 무한반복
-            System.out.println("숫자 연산자 숫자 순으로 입력하세요.(입력값 공백으로 구분) "); // 입력 한번에 받음
-            int firstNumber = sc.nextInt(); //nextInt로 숫자 받아서 초기화
-            char operator = sc.next().charAt(0);//sc.next()로 문자열을 받은 후 0번째 char를 가져옴
-            int secondNumber = sc.nextInt();
             try {
-                int result = calcInstance.calculate(firstNumber,secondNumber,operator);//정수, 연산 기호를 인자로 넘겨서 calculate 메소드 호출
-                System.out.printf("%d %c %d 결과 : %d\n", firstNumber, operator, secondNumber, result);
-                System.out.println("remove 0번째 결과 삭제 | inquiry 결과 값 조회 (exit 입력 시 종료)"); 
-                String func = sc.next(); // sc.next()로 비교하면 입력 버퍼의 문자열이 사라지므로 String에 담아서 입력값 확인
-                if(func.equals("exit")){
-                    break;
-                } else if(func.equals("remove")){
-                    calcInstance.removeResults();//removeResults 메소드를 이용해 삭제 수행
-                } else if(func.equals("inquiry")){ // inquiry 기능 구현
-                    calcInstance.inquiryResults(); // Calculator클래스의 inquiryResults 메소드를 이용해 결과 출력
+                System.out.println("사칙연산 1 | 원의 넓이 계산 2 (숫자 입력)");
+                int feature = sc.nextInt();
+                if(feature > 2) {
+                    throw new IllegalArgumentException("1과 2중에 입력해주세요");
                 }
-                sc.nextLine(); // 다음 반복문을 위해 스캐너 비워줌
-            } catch (ArithmeticException e) {
+
+                if(feature == 1) {
+                    System.out.println("숫자 연산자 숫자 순으로 입력하세요.(입력값 공백으로 구분) "); // 입력 한번에 받음
+                    int firstNumber = sc.nextInt(); //nextInt로 숫자 받아서 초기화
+                    char operator = sc.next().charAt(0);//sc.next()로 문자열을 받은 후 0번째 char를 가져옴
+                    int secondNumber = sc.nextInt();
+                        int result = calcInstance.calculate(firstNumber, secondNumber,
+                            operator);//정수, 연산 기호를 인자로 넘겨서 calculate 메소드 호출
+                        System.out.printf("%d %c %d 결과 : %d\n", firstNumber, operator, secondNumber,
+                            result);
+                        System.out.println("remove 0번째 결과 삭제 | inquiry 결과 값 조회 (exit 입력 시 종료)");
+                        String func = sc.next(); // sc.next()로 비교하면 입력 버퍼의 문자열이 사라지므로 String에 담아서 입력값 확인
+                        if (func.equals("exit")) {
+                            break;
+                        } else if (func.equals("remove")) {
+                            calcInstance.removeResults();//removeResults 메소드를 이용해 삭제 수행
+                        } else if (func.equals("inquiry")) { // inquiry 기능 구현
+                            calcInstance.inquiryResults(); // Calculator클래스의 inquiryResults 메소드를 이용해 결과 출력
+                        }
+                        sc.nextLine(); // 다음 반복문을 위해 스캐너 비워줌
+                } else if(feature == 2){ // 2입력시 원의 넓이 구하기 수행
+                    System.out.println("원의 반지름을 입력하세요");
+                    int radius = sc.nextInt();
+                    calcInstance.getAreaResults().add(calcInstance.calculateCircle(radius)); // 원의 넓이 저장
+                    calcInstance.inquiryAreaResults(); // 결과 조회
+                    System.out.println("exit 입력 시 종료");
+                    String func = sc.next();
+                    if (func.equals("exit")) {
+                        break;
+                    }
+                    sc.nextLine(); // 다음 반복문을 위해 스캐너 비워줌
+                }
+            } catch (Exception e) {
                 System.out.println(e.getMessage());//e.getmessage()를 이용해 throw한 예외의 오류메세지 출력
-            } catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
             }//try-catch문 활용해서 예외 처리
         }
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,21 +3,32 @@ package calculator;
 import java.util.ArrayList;
 
 public class Calculator {
+  static final double PI = 3.14; //원주율은 변하지 않는 상수이기 때문에 final로 선언, static으로 선언해 모든 calculator 인스턴스가 동일한 PI를 사용하게 되어 메모리 절약
   private ArrayList<Integer> results; //생성자를 통해 초기화 하기 위해 선언만 해줌
+  private ArrayList<Double> areaResults; // 원의 넓이를 저장할 컬렉션 areaResults선언
   Calculator(){ // Calculator 클래스 생성자
     results = new ArrayList<>(); // 생성자 통해 results초기화
+    areaResults = new ArrayList<>(); // areaResult초기화
   }
   public ArrayList<Integer> getResults() { // getResults() 메소드를 통해 외부에서 results에 접근할 수 있게 함
-    return results;
+    return this.results;
   }
   public void setResults(ArrayList<Integer> result) { //setResults()메소드를 이용해 results 수정할 수 있게 함
     this.results = result;
   }
+
+  public void setAreaResults(ArrayList<Double> areaResults) { // areaResult Setter 구현
+    this.areaResults = areaResults;
+  }
+  public ArrayList<Double> getAreaResults(){ // areaResult Getter구현
+    return this.areaResults;
+  }
+
   public void removeResults(){ // 반환타입이 없기 때문에 void로 선언
     this.results.remove(0); // 메소드 호출시 index 0번째의 값 삭제
   }
   public void inquiryResults(){ // results를 조회하는 메소드
-    for(int i : results){ // results 컬렉션을 순회하면서 출력
+    for(int i : this.results){ // results 컬렉션을 순회하면서 출력
       System.out.printf("%d ",i);
     }
     System.out.println();
@@ -40,5 +51,14 @@ public class Calculator {
     }
     this.results.add(result); // calculator 클래스 안의 컬렉션에 저장
     return result;
+  }
+  public void inquiryAreaResults(){ // arearesults를 조회하는 메소드
+    for(double i : this.areaResults){ // arearesults 컬렉션을 순회하면서 출력
+      System.out.printf("%.2f ",i);
+    }
+    System.out.println();
+  }
+  public double calculateCircle(int radius){ //원의 넓이를 계산해주는 메소드
+    return PI * radius * radius;
   }
 }
